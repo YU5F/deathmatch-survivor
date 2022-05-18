fba84c96283d61c74ae78ffb58bc0d96dad971d5
YU5F <87546692+YU5F@users.noreply.github.com>
2022-05-18 21:31:25 +0300

    Added Enemy

---
+++ Bloody Blade/Assets/Scripts/Enemy/EnemyStats.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class EnemyStats : MonoBehaviour
+{
+    public int enemyHealth = 100;
+    void Update(){
+        Debug.Log(enemyHealth);
+    }
+}
--- Bloody Blade/Assets/Scripts/Player/Bullet.cs
+++ Bloody Blade/Assets/Scripts/Player/Bullet.cs
@@ -4,9 +4,12 @@ public class Bullet : MonoBehaviour
 {
     private GameObject player;
     [SerializeField, Range(10f, 50f)] private float maxRange = 30.0f;
+    [SerializeField, Range(1, 100)] private int damage = 10;
+    private EnemyStats enemy;
 
     void Awake(){
         player = GameObject.Find("Player");
+        enemy = GameObject.Find("Enemy").GetComponent<EnemyStats>();
     }
     void Update(){
         float distance = Vector3.Distance(transform.position, player.transform.position);
@@ -17,6 +20,13 @@ public class Bullet : MonoBehaviour
     void OnCollisionEnter2D(Collision2D collision){
         if(!collision.gameObject.CompareTag("player")){
             Destroy(gameObject);
+        }
+        if(collision.gameObject.CompareTag("enemy")){
+            enemy.enemyHealth -= damage;
+            Destroy(gameObject);
+            if(enemy.enemyHealth <= 0){
+                Destroy(collision.gameObject);
+            }
         }
     }
 }
