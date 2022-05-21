dc3573791cdfd96e1b620e3a3074dc38dd27759e
YU5F <87546692+YU5F@users.noreply.github.com>
2022-05-21 18:49:27 +0300

    Added Enemy Pathfinding

---
+++ Bloody Blade/Assets/Scripts/Enemy/EnemyAI.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyAI : MonoBehaviour
+{
+    private Transform target;
+    [SerializeField] private float speed = 500f;
+    public float nextWaypointDistance = 3f;
+
+    Path path;
+    int currentWaypoint = 0;
+    bool reachedEndOfPath = false;
+
+    Seeker seeker;
+    Rigidbody2D enemyRb;
+
+    void OnPathComplete(Path p){
+        if(!p.error){
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+    void Start()
+    {
+        target = GameObject.Find("Player").GetComponent<Transform>();
+        seeker = GetComponent<Seeker>();
+        enemyRb = GetComponent<Rigidbody2D>();
+
+        InvokeRepeating("UpdatePath", 0f, .5f);        
+    }
+
+    void UpdatePath(){
+        if(seeker.IsDone())
+            seeker.StartPath(enemyRb.position, target.position, OnPathComplete);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(path == null)
+            return;
+
+        if(currentWaypoint >= path.vectorPath.Count){
+            reachedEndOfPath = true;
+            return;
+        }
+        else{
+            reachedEndOfPath = false;
+        }
+
+        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - enemyRb.position).normalized;
+        Vector2 force = direction * speed * Time.deltaTime;
+
+        enemyRb.AddForce(force);
+
+        float distance = Vector2.Distance(enemyRb.position, path.vectorPath[currentWaypoint]);
+        if(distance < nextWaypointDistance){
+            currentWaypoint++;
+        }
+    }
+}
--- Bloody Blade/Assets/Scripts/Player/Controllers/AIController.cs
+++ Bloody Blade/Assets/Scripts/Player/Controllers/AIController.cs
@@ -20,7 +20,7 @@ public class AIController : inputController
 
     public override bool retrieveFireInput()
     {
-        return true;
+        return false;
     }
 
     public override float retrieveVerticalMoveInput()
